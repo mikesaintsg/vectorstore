6a497ff3df90538354722689c7c03ed430d2c98e
mikesaintsg <45922630+mikesaintsg@users.noreply.github.com>
2026-01-18 16:13:48 -0500

    Rename library from 'adapters' to 'vectorstore'

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
-			name: 'adapters',
+			name: 'vectorstore',
 			fileName: 'index',
 			formats: ['es'],
 		},
